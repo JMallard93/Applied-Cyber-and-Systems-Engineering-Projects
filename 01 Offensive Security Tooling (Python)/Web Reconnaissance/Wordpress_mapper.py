f4efc5eb6df41a969d1895500b6b7836ec1432f7
JMallard93 <mallard.bsn@gmail.com>
2026-09-16 11:36:37 -0400

    Refactor Wordpress_mapper.py for clarity and functionality
    Wordpress web content scanner that traverses a local directory structure, strips out static media files, and uses multiple concurrent threads to test if those same file paths exist and are accessible on a remote target web server.

--- 01 Offensive Security Tooling (Python)/Web Reconnaissance/Wordpress_mapper.py
+++ 01 Offensive Security Tooling (Python)/Web Reconnaissance/Wordpress_mapper.py
@@ -1,3 +1,6 @@
+# Wordpress web content scanner that traverses a local directory structure, strips out static media files, 
+# and uses multiple concurrent threads to test if those same file paths exist and are accessible on a remote target web server.
+
 import contextlib
 import os
 import queue
@@ -6,49 +9,51 @@ import sys
 import threading
 import time
 
-FILTERED = [".jpg", ".gif", ".png", ".css"]
-TARGET = "http://boodelyboo.com/wordpress"
+FILTERED = [".jpg", ".gif", ".png", ".css"] # a list of file extensions to ignore
+TARGET = "http://boodelyboo.com/wordpress" 
 THREADS = 10
 
+# initialize thread-safe first in, first out queues to store discovered URLs and paths
 answers = queue.Queue()
 web_paths = queue.Queue()
 
+# gather local files
 def gather_paths():
-    for root, _, files in os.walk('.'):
-        for fname in files:
-            if os.path.splitext(fname)[1] in FILTERED:
+    for root, _, files in os.walk('.'): # talk recursively through all directories/files, starting at the current directory
+        for fname in files: # loop through all files found
+            if os.path.splitext(fname)[1] in FILTERED: # ignore files that have extension in the FILTERED list
                 continue
-            path = os.path.join(root, fname)
-            if path.startswith('.'):
+            path = os.path.join(root, fname) # build the full path to the file
+            if path.startswith('.'): # formatting, remove dots from the beginning of paths
                 path = path[1:]
             print(path)
-            web_paths.put(path)
+            web_paths.put(path) # push the path into the web_paths queue to be used later
 
-@contextlib.contextmanager
+# directory context manager
+@contextlib.contextmanager # decorator that will allow the creation of enter and exit methods in a context manager
 def chdir(path):
-    """
-    On enter, change directory to specified path.
-    On exit, change directory back to original
-    """
-    this_dir = os.getcwd()
-    os.chdir(path)
+    # On enter, change directory to specified path.
+    # On exit, change directory back to original
+    this_dir = os.getcwd() # saves the cwd  
+    os.chdir(path) # change into the target directory
     try:
-        yield
+        yield # pause the function midway
     finally:
-        os.chdir(this_dir)
+        os.chdir(this_dir) # no matter what happens, change the working directory back to where it started
 
+# test the server, each thread will run this function
 def test_remote():
-    while not web_paths.empty():
-        path = web_path.get()
-        url = f'{TARGET}{PATH}'
-        time.sleep(2) # target may have a throttling/lockout
-        r = requests.get(url)
-        if r.status_code == 200:
-            answers.put(url)
-            sys.stdout.write('+')
+    while not web_paths.empty(): # continue looping while there are paths in the queue
+        path = web_paths.get() # pull a path from the queue
+        url = f'{TARGET}{path}' # combine the target url with the path
+        time.sleep(2) # target may have a throttling/lockout timer, or a WAF
+        r = requests.get(url) # send GET request to target url
+        if r.status_code == 200: # 200 OK status code means file exists and is accessible
+            answers.put(url) # saves valid url into 'answers' queue
+            sys.stdout.write('+') # print '+' to indicate successful find
         else:
-            sys.stdout.write('x')
-        sys.stdout.flush()
+            sys.stdout.write('x') # indicates the url did not exist or was not accessible
+        sys.stdout.flush() # force the output to render immediately without waiting for a newline
 
 def run():
     mythreads = list()
