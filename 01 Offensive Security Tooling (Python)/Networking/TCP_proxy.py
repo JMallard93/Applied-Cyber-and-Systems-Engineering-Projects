d96cd4db22771125528aa5c0f61eedc3228f2140
JMallard93 <mallard.bsn@gmail.com>
2026-09-17 14:47:32 -0400

    Implement TCP proxy for traffic interception and modification
    This script implements a custom network proxy that intercepts, inspects, hex-dumps, and relays TCP traffic between a local client and a remote server, allowing for real-time packet modification and analysis.

--- 01 Offensive Security Tooling (Python)/Networking/TCP_proxy.py
+++ 01 Offensive Security Tooling (Python)/Networking/TCP_proxy.py
@@ -1,67 +1,73 @@
+# Custom network proxy that intercepts, inspects, hex-dumps, and relays TCP traffic between a local client
+# and a remote server, allowing for real-time packet modification and analysis.
+
 import sys
 import socket
 import threading
 
+# filters each ASCII value, if it's printable then keep it, otherwise replace non-printable bytes with dots for readability
 HEX_FILTER = ''.join(
 	[(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
-
+# formats binary/string data into standard hexadecimal and printable text
 def hexdump(src, length=16, show=True):
-	if isinstance(src, bytes):
+	if isinstance(src, bytes): # converts byte data into a string if necessary
 		src = src.decode()
 
-	results = list()
-	for i in range(0, len(src), length):
-		word = str(src[i:i+length])
+	results = list() # initialize a list for the formatted lines of the hexdump
+	for i in range(0, len(src), length): # loops through the data in 16 byte chunks
+		word = str(src[i:i+length]) # turns the chunk of data into a string
 
-		printable = word.translate(HEX_FILTER)
-		hexa = ' '.join([f'{ord(c):02X}' for c in word])
-		hexwidth = length*3
-		results.append(f'{i:04x}  {hexa:<{hexwidth}} {printable}')
+		printable = word.translate(HEX_FILTER) # if it's a non-printable character, turn it into a dot using HEX_FILTER
+		hexa = ' '.join([f'{ord(c):02X}' for c in word]) # converts each character into into it's hexadecimal
+		hexwidth = length*3 # formatting
+		results.append(f'{i:04x}  {hexa:<{hexwidth}} {printable}') # puts the results into one row
+	# if show is True, print each row to the console. Otherwise return the list of formatted strings
 	if show:
 		for line in results:
 			print(line)
 	else:
 		return results
 
-
+# response modification handler
 def receive_from(connection):
-	buffer = b""
-	connection.settimeout(5)
+	buffer = b"" # initialize an empty byte string buffer
+	connection.settimeout(5) # set a 5 second timeout for if the sender stops talking
 	try:
-		while True:
-			data = connection.recv(4096)
-			if not data:
+		while True: # continuously look for data from the socket
+			data = connection.recv(4096) # read 4096 bytes at a time, creating a chunk
+			if not data: # break out of the loop if the connection is closed
 				break
-			buffer += data
-	except Exception as e:
+			buffer += data # appends the data chunk to our buffer
+	except Exception as e: # silently catch timeouts or socket errors
 		pass
 	return buffer
 
-
+# packet handler, add code to modify packet
 def request_handler(buffer):
 	# perform packet modifications
 	return buffer
 
-
+# packet handler, add code to modify packet
 def response_handler(buffer):
 	# perform packet modifications
 	return buffer
 
-
+# core proxy function
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
-	remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	remote_socket.connect((remote_host, remote_port))
+	remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create new TCP socket
+	remote_socket.connect((remote_host, remote_port)) # establist connection to remote host
 
-	if receive_first:
-		remote_buffer = receive_from(remote_socket)
-		hexdump(remote_buffer)
+	if receive_first: # checks if we need to grab a greeting banner from remote server before waiting for client (protocol dependent)
+		remote_buffer = receive_from(remote_socket) # pulls initial data
+		hexdump(remote_buffer) # prints hex dump of that data
 
-	remote_buffer = response_handler(remote_buffer)
-	if len(remote_buffer):
-		print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
-		client_socket.send(remote_buffer)
+	remote_buffer = response_handler(remote_buffer) # modify the remote buffer with response_handler()
+	if len(remote_buffer): # if data was received 
+		print("[<==] Sending %d bytes to localhost." % len(remote_buffer)) # log that there was a response (which is the print statement)
+		client_socket.send(remote_buffer) # and send to the client
 
+	# loop for packet relay
 	while True:
 		local_buffer = receive_from(client_socket)
 		if len(local_buffer):
